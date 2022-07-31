5cac891151f245d74e460acbabc0520fb780c2ab
Neel Choksi <neelchoksiwork@gmail.com>
2022-07-31 23:03:19 +0530

    added docs

--- 1_tries/2-try/React1/client/src/components/Products/Catelog/Catelog.js
+++ 1_tries/2-try/React1/client/src/components/Products/Catelog/Catelog.js
@@ -1,32 +1,49 @@
-import React,{useState} from 'react';
-import axios from 'axios';
-import {saveAs} from 'file-saver';
+import React, { useState } from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
 
-const Catelog = () =>{
-
-  return(
+const Catelog = () => {
+  const handleChange = () => {};
+  const createAndDownloadPdf = ()=>{};
+  return (
     <>
-    <h1>Catelog</h1>
-    <div>
-      <ul>
-        <li>view product with checkbox option to select in pdf</li>
-        <li>select product to print pdf</li>
-      </ul>
-    </div>
+      <h1>Catelog</h1>
+      <div>
+        <ul>
+          <li>view product with checkbox option to select in pdf</li>
+          <li>select product to print pdf</li>
+        </ul>
+      </div>
 
-    <div className="products-printing">
-      <input type="text" placeholder="name" name="name" onChange={handleChange} />
-      <input type="number" placeholder="receipt id" name="receiptId" onChange={handleChange} />
-      <input type="number" placeholder="price1" name="price1" onChange={handleChange} />
-      <input type="number" placeholder="price2" name="price2" onChange={handleChange} />
-      <button onClick={createAndDownloadPdf}>
-        Download PDF
-      </button>
-    </div>
-
-
+      <div className="products-printing">
+        <input
+          type="text"
+          placeholder="name"
+          name="name"
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="receipt id"
+          name="receiptId"
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="price1"
+          name="price1"
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="price2"
+          name="price2"
+          onChange={handleChange}
+        />
+        <button onClick={createAndDownloadPdf}>Download PDF</button>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Catelog;
